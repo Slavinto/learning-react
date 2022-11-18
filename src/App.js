e76689afacdc4cd95f8d1bd780f76abfd97ebb43
slavinto <slava3669@gmail.com>
2022-11-18 15:30:31 +0300

    reading rendering-lists

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 // now learning:
+// V https://beta.reactjs.org/learn/thinking-in-react
+// V https://beta.reactjs.org/learn/your-first-component
+// V https://beta.reactjs.org/learn/importing-and-exporting-components
+// V https://beta.reactjs.org/learn/writing-markup-with-jsx
+// V https://beta.reactjs.org/learn/javascript-in-jsx-with-curly-braces
+// V https://beta.reactjs.org/learn/passing-props-to-a-component
+// V https://beta.reactjs.org/learn/conditional-rendering
+// V https://beta.reactjs.org/learn/rendering-lists
+// ?? https://beta.reactjs.org/learn/extracting-state-logic-into-a-reducer
 // https://beta.reactjs.org/learn/adding-interactivity
-// https://beta.reactjs.org/learn/thinking-in-react
 // https://beta.reactjs.org/learn/managing-state
+// https://beta.reactjs.org/learn/passing-data-deeply-with-context
 
 import logo from "./logo.svg";
 import "./App.css";
@@ -12,3 +21,7 @@ function App() {
 }
 
 export default App;
+
+// ===============================================================
+
+// ===============================================================
